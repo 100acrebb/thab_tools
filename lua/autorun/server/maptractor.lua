8f376b070400d2f38992d5b5b62b36ec8a5cdbb1
Mike Zeoli <mzeoli.it@gmail.com>
2016-04-20 13:03:16 -0400

    added license

--- lua/autorun/server/maptractor.lua
+++ lua/autorun/server/maptractor.lua
@@ -34,8 +34,21 @@ To Do:
   Build out an optional routine to load the most popular map(s) when the server goes empty.
   Eliminate the need for ULX for the !mapstats command
   Incorporate peak time / off time concepts, so maps don't get penalized for running during off-hours, etc.
+  
+  
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
+
 
 if SERVER then
 
